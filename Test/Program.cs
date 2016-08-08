a5ea4c08074c4babb68d345c9f1bf99f1573dd5e
freeyxm <freeyxm@gmail.com>
2016-08-08 12:31:10 +0800

    Add Test project.

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CsNet.Util;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AvlTreeTest test = new AvlTreeTest();
+            test.TestValidity();
+            test.TestPerformace();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test/TestUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Test
+{
+    class TestUtility
+    {
+        public static void RunTime(string title, Stopwatch watch, Action action)
+        {
+            Console.Write("Start {0} ...", title);
+            watch.Restart();
+            action();
+            watch.Stop();
+            Console.WriteLine(" {0} ms", watch.ElapsedMilliseconds);
+        }
+    }
+}
