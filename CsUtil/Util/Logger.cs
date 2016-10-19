21c4c1bb340494cb91db313df5055193e598f37c
freeyxm <freeyxm@gmail.com>
2016-10-19 10:16:11 +0800

    Add Logger.

---
+++ CsUtil/Util/Logger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CsUtil.Util
+{
+    public class Logger
+    {
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Warning,
+            Error,
+        }
+        public static LogLevel logLevel { get; set; }
+
+        private static void Write(LogLevel level, string msg)
+        {
+            Console.WriteLine("[{0}] {1}", level, msg);
+        }
+
+        public static void Log(LogLevel level, string msg)
+        {
+            if (logLevel <= level)
+            {
+                Write(level, msg);
+            }
+        }
+
+        public static void Log(LogLevel level, string format, params object[] args)
+        {
+            if (logLevel <= level)
+            {
+                if (args.Length > 0)
+                    format = string.Format(format, args);
+                Write(level, format);
+            }
+        }
+
+        public static void Debug(string msg)
+        {
+            Log(LogLevel.Debug, msg);
+        }
+
+        public static void Debug(string format, params object[] args)
+        {
+            Log(LogLevel.Debug, format, args);
+        }
+
+        public static void Info(string msg)
+        {
+            Log(LogLevel.Info, msg);
+        }
+
+        public static void Info(string format, params object[] args)
+        {
+            Log(LogLevel.Info, format, args);
+        }
+
+        public static void Warning(string msg)
+        {
+            Log(LogLevel.Warning, msg);
+        }
+
+        public static void Warning(string format, params object[] args)
+        {
+            Log(LogLevel.Warning, format, args);
+        }
+
+        public static void Error(string msg)
+        {
+            Log(LogLevel.Error, msg);
+        }
+
+        public static void Error(string format, params object[] args)
+        {
+            Log(LogLevel.Error, format, args);
+        }
+    }
+}
