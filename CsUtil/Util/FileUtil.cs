fa964567df02ba7f251f724b5a5fa812ee328955
freeyxm <freeyxm@gmail.com>
2016-10-28 16:59:56 +0800

    Update FileUtil.

--- CsUtil/Util/FileUtil.cs
+++ CsUtil/Util/FileUtil.cs
@@ -6,9 +6,108 @@ namespace CsUtil.Util
 {
     public class FileUtil
     {
+        public static bool OpenFile(string path, FileMode mode, Action<FileStream> action)
+        {
+            try
+            {
+                FileStream stream = new FileStream(path, mode);
+                try
+                {
+                    action(stream);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e.Message);
+                    return false;
+                }
+                finally
+                {
+                    stream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e.Message);
+                return false;
+            }
+        }
+
+        public static bool OpenFile(string path, FileMode mode, Func<FileStream, bool> action)
+        {
+            try
+            {
+                FileStream stream = new FileStream(path, mode);
+                try
+                {
+                    return action(stream);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e.Message);
+                    return false;
+                }
+                finally
+                {
+                    stream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e.Message);
+                return false;
+            }
+        }
+
+        public static bool OpenFile(string path, Func<string, FileStream> streamCreater, Func<FileStream, bool> action)
+        {
+            try
+            {
+                FileStream stream = streamCreater(path);
+                try
+                {
+                    return action(stream);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e.Message);
+                    return false;
+                }
+                finally
+                {
+                    if (stream != null) stream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e.Message);
+                return false;
+            }
+        }
+
+        public static bool OpenFile(string path, Action<FileStream> action)
+        {
+            return OpenFile(path, FileMode.Open, action);
+        }
+
+        public static bool OpenFile(string path, Func<FileStream, bool> action)
+        {
+            return OpenFile(path, FileMode.Open, action);
+        }
+
+        public static bool OpenFile(string inFile, string outFile, Func<FileStream, FileStream, bool> action)
+        {
+            return OpenFile(inFile, FileMode.Open, (inStream) =>
+            {
+                return OpenFile(outFile, FileMode.Create, (outStream) =>
+                {
+                    return action(inStream, outStream);
+                });
+            });
+        }
+
         public static bool WriteFile(string path, byte[] data)
         {
-            FileStream stream = null;
             try
             {
                 string dir = Directory.GetParent(path).FullName;
@@ -17,53 +116,17 @@ namespace CsUtil.Util
                     Directory.CreateDirectory(dir);
                 }
 
-                stream = new FileStream(path, FileMode.Create);
-                stream.Write(data, 0, data.Length);
-                stream.Flush();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Logger.Error("WriteFile error: path = '{0}', error = {0}", path, e.Message);
-                return false;
-            }
-            finally
-            {
-                if (stream != null) stream.Close();
-            }
-        }
-
-        public static bool OpenFile(string path, System.Action<FileStream> action)
-        {
-            FileStream stream = null;
-            try
-            {
-                stream = new FileStream(path, FileMode.Open);
-                action(stream);
-                return true;
-            }
-            catch (Exception e)
-            {
-                Logger.Error("OpenFile error: path = '{0}', error = {0}", path, e.Message);
-                return false;
-            }
-            finally
-            {
-                if (stream != null) stream.Close();
-            }
-        }
-
-        public static string ReadFile(string path)
-        {
-            string result = null;
-            OpenFile(path, (stream) =>
-            {
-                using (StreamReader reader = new StreamReader(stream))
+                return OpenFile(path, FileMode.Create, (stream) =>
                 {
-                    result = reader.ReadToEnd();
-                }
-            });
-            return result;
+                    stream.Write(data, 0, data.Length);
+                    stream.Flush();
+                });
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e.Message);
+                return false;
+            }
         }
 
         public static bool DeleteFile(string path)
@@ -78,9 +141,22 @@ namespace CsUtil.Util
             }
             catch (Exception e)
             {
-                Logger.Error("DeleteFile error: path = '{0}', error = {0}", path, e.Message);
+                Logger.Error(e.Message);
                 return false;
             }
+        }
+
+        public static string ReadFile(string path)
+        {
+            string result = null;
+            OpenFile(path, (stream) =>
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            });
+            return result;
         }
 
         /// <summary>
