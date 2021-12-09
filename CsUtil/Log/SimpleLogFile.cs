9ae06248bec61c312f781cb8521fa8e172b7255a
freeyxm <freeyxm@live.com>
2021-12-09 15:42:39 +0800

    Update SimpleLogFile.

--- CsUtil/Log/SimpleLogFile.cs
+++ CsUtil/Log/SimpleLogFile.cs
@@ -7,9 +7,14 @@ namespace CsUtil.Log
     public class SimpleLogFile
     {
         private StreamWriter m_streamWriter;
+        private string m_path;
+
+        public string Path { get { return m_path; } }
 
         public bool Open(string path)
         {
+            m_path = path;
+
             Close();
 
             try
@@ -66,6 +71,14 @@ namespace CsUtil.Log
             }
         }
 
+        public void WriteLine(string format, params object[] args)
+        {
+            if (m_streamWriter != null)
+            {
+                m_streamWriter.WriteLine(format, args);
+            }
+        }
+
         public void WriteLine()
         {
             if (m_streamWriter != null)
@@ -93,6 +106,12 @@ namespace CsUtil.Log
             return this;
         }
 
+        public SimpleLogFile AppendLine(string format, params object[] args)
+        {
+            WriteLine(format, args);
+            return this;
+        }
+
         public SimpleLogFile AppendLine()
         {
             WriteLine();
@@ -100,22 +119,59 @@ namespace CsUtil.Log
         }
 
 
+        public void Log(string content)
+        {
+            Append("[-Debug-] ").AppendLine(content);
+        }
+
         public void LogFormat(string format, params object[] args)
         {
-            string content = string.Format(format, args);
-            Append("[-Debug-] ").AppendLine(content).AppendLine();
+            Append("[-Debug-] ").AppendLine(format, args);
+        }
+
+        public void LogError(string content)
+        {
+            Append("[-Error-] ").AppendLine(content);
         }
 
         public void LogErrorFormat(string format, params object[] args)
         {
-            string content = string.Format(format, args);
-            Append("[-Error-] ").AppendLine(content).AppendLine();
+            Append("[-Error-] ").AppendLine(format, args);
+        }
+
+        public void LogWarning(string content)
+        {
+            Append("[Warning] ").AppendLine(content);
         }
 
         public void LogWarningFormat(string format, params object[] args)
         {
-            string content = string.Format(format, args);
-            Append("[Warning] ").AppendLine(content).AppendLine();
+            Append("[Warning] ").AppendLine(format, args);
+        }
+
+
+        public static bool OpenLogFile(string path, string namePrefix, out SimpleLogFile logFile)
+        {
+            try
+            {
+                logFile = new SimpleLogFile();
+
+                path = string.Format("{0}/{1}_{2}.log", path, namePrefix, DateTime.Now.ToString("yyyMMdd-HHmmss.ffff"));
+
+                string dir = System.IO.Path.GetDirectoryName(path);
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                return logFile.Open(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                logFile = null;
+                return false;
+            }
         }
     }
 }
