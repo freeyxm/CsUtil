33ad795d8d79bbb7ebfc760fec4bbb32b10a0652
freeyxm <freeyxm@live.com>
2022-03-23 10:39:24 +0800

    Update SimpleLogFile.

--- CsUtil/Log/SimpleLogFile.cs
+++ CsUtil/Log/SimpleLogFile.cs
@@ -8,8 +8,7 @@ namespace CsUtil.Log
     {
         private StreamWriter m_streamWriter;
         private string m_path;
-
-        public string Path { get { return m_path; } }
+        public string Path => m_path;
 
         public bool Open(string path)
         {
@@ -46,109 +45,221 @@ namespace CsUtil.Log
             }
         }
 
+        #region Write
 
-        public void Write(string content)
+        public void Write(string value)
         {
-            if (m_streamWriter != null)
-            {
-                m_streamWriter.Write(content);
-            }
+            m_streamWriter.Write(value);
         }
 
         public void Write(string format, params object[] args)
         {
-            if (m_streamWriter != null)
-            {
-                m_streamWriter.Write(format, args);
-            }
+            m_streamWriter.Write(format, args);
         }
 
-        public void WriteLine(string content)
+        public void WriteLine(string value)
         {
-            if (m_streamWriter != null)
-            {
-                m_streamWriter.WriteLine(content);
-            }
+            m_streamWriter.WriteLine(value);
         }
 
         public void WriteLine(string format, params object[] args)
         {
-            if (m_streamWriter != null)
-            {
-                m_streamWriter.WriteLine(format, args);
-            }
+            m_streamWriter.WriteLine(format, args);
         }
 
         public void WriteLine()
         {
-            if (m_streamWriter != null)
-            {
-                m_streamWriter.WriteLine();
-            }
+            m_streamWriter.WriteLine();
         }
 
+        #endregion
 
-        public SimpleLogFile Append(string content)
+        #region Append
+
+        public SimpleLogFile Append(int value)
         {
-            Write(content);
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(uint value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(long value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(ulong value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(float value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(double value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(decimal value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(bool value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(char value)
+        {
+            m_streamWriter.Write(value);
+            return this;
+        }
+
+        public SimpleLogFile Append(char[] buffer)
+        {
+            m_streamWriter.Write(buffer);
+            return this;
+        }
+
+        public SimpleLogFile Append(char[] buffer, int index, int count)
+        {
+            m_streamWriter.Write(buffer, index, count);
+            return this;
+        }
+
+        public SimpleLogFile Append(string value)
+        {
+            m_streamWriter.Write(value);
             return this;
         }
 
         public SimpleLogFile Append(string format, params object[] args)
         {
-            Write(format, args);
+            m_streamWriter.Write(format, args);
             return this;
         }
 
-        public SimpleLogFile AppendLine(string content)
+        public SimpleLogFile AppendLine(string value)
         {
-            WriteLine(content);
+            m_streamWriter.WriteLine(value);
             return this;
         }
 
         public SimpleLogFile AppendLine(string format, params object[] args)
         {
-            WriteLine(format, args);
+            m_streamWriter.WriteLine(format, args);
             return this;
         }
 
         public SimpleLogFile AppendLine()
         {
-            WriteLine();
+            m_streamWriter.WriteLine();
             return this;
         }
 
+        #endregion
 
-        public void Log(string content)
+        #region Unity Like
+
+        public void Log(string value)
         {
-            Append("[-Debug-] ").AppendLine(content);
+            string time = GetTime();
+#if UNITY_EDITOR
+            UnityEngine.Debug.LogFormat("{0} [f:{1}] {2}", time, UnityEngine.Time.frameCount, value);
+#endif
+            Append(time);
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IPHONE
+            Append(" [f:").Append(UnityEngine.Time.frameCount).Append("]");
+#endif
+            Append(" [-Debug-] ").AppendLine(value);
         }
 
         public void LogFormat(string format, params object[] args)
         {
-            Append("[-Debug-] ").AppendLine(format, args);
+            string time = GetTime();
+#if UNITY_EDITOR
+            UnityEngine.Debug.LogFormat("{0} [f:{1}] {2}", time, UnityEngine.Time.frameCount, string.Format(format, args));
+#endif
+            Append(time);
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IPHONE
+            Append(" [f:").Append(UnityEngine.Time.frameCount).Append("]");
+#endif
+            Append(" [-Debug-] ").AppendLine(format, args);
         }
 
-        public void LogError(string content)
+        public void LogError(string value)
         {
-            Append("[-Error-] ").AppendLine(content);
+            string time = GetTime();
+#if UNITY_EDITOR
+            UnityEngine.Debug.LogErrorFormat("{0} [f:{1}] {2}", time, UnityEngine.Time.frameCount, value);
+#endif
+            Append(time);
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IPHONE
+            Append(" [f:").Append(UnityEngine.Time.frameCount).Append("]");
+#endif
+            Append(" [-Error-] ").AppendLine(value);
         }
 
         public void LogErrorFormat(string format, params object[] args)
         {
-            Append("[-Error-] ").AppendLine(format, args);
+            string time = GetTime();
+#if UNITY_EDITOR
+            UnityEngine.Debug.LogErrorFormat("{0} [f:{1}] {2}", time, UnityEngine.Time.frameCount, string.Format(format, args));
+#endif
+            Append(time);
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IPHONE
+            Append(" [f:").Append(UnityEngine.Time.frameCount).Append("]");
+#endif
+            Append(" [-Error-] ").AppendLine(format, args);
         }
 
-        public void LogWarning(string content)
+        public void LogWarning(string value)
         {
-            Append("[Warning] ").AppendLine(content);
+            string time = GetTime();
+#if UNITY_EDITOR
+            UnityEngine.Debug.LogWarningFormat("{0} [f:{1}] {2}", time, UnityEngine.Time.frameCount, value);
+#endif
+            Append(time);
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IPHONE
+            Append(" [f:").Append(UnityEngine.Time.frameCount).Append("]");
+#endif
+            Append(" [Warning] ").AppendLine(value);
         }
 
         public void LogWarningFormat(string format, params object[] args)
         {
-            Append("[Warning] ").AppendLine(format, args);
+            string time = GetTime();
+#if UNITY_EDITOR
+            UnityEngine.Debug.LogWarningFormat("{0} [f:{1}] {2}", time, UnityEngine.Time.frameCount, string.Format(format, args));
+#endif
+            Append(time);
+#if UNITY_EDITOR || UNITY_ANDROID || UNITY_IPHONE
+            Append(" [f:").Append(UnityEngine.Time.frameCount).Append("]");
+#endif
+            Append(" [Warning] ").AppendLine(format, args);
         }
 
+        #endregion
+
+        private string GetTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+        }
 
         public static bool OpenLogFile(string path, string namePrefix, out SimpleLogFile logFile)
         {
@@ -156,7 +267,7 @@ namespace CsUtil.Log
             {
                 logFile = new SimpleLogFile();
 
-                path = string.Format("{0}/{1}_{2}.log", path, namePrefix, DateTime.Now.ToString("yyyMMdd-HHmmss.ffff"));
+                path = string.Format("{0}/{1}_{2}.log", path, namePrefix, DateTime.Now.ToString("yyyyMMdd-HHmmss-fff"));
 
                 string dir = System.IO.Path.GetDirectoryName(path);
                 if (!Directory.Exists(dir))
